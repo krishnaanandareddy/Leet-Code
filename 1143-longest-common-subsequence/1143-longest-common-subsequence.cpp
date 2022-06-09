87d92f35b49af703da92b9ee5fed71bd673dc886
krishnaanandareddy <84138720+krishnaanandareddy@users.noreply.github.com>
2022-06-09 07:38:47 +0530

    Time: 33 ms (48.08%), Space: 13 MB (40.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     int longestCommonSubsequence(string &a, string &b) {
-        short m[1001][1001] = {};
+        int n1 = a.size(), n2 = b.size();
+        vector<vector<int>> m(n1+1,vector<int>(n2+1,0));
         for (auto i = 0; i < a.size(); ++i)
             for (auto j = 0; j < b.size(); ++j)
                 m[i + 1][j + 1] = a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
-        return m[a.size()][b.size()];
+        int n = m[a.size()][b.size()];
+        int i=n1,j=n2,index = n-1;
+        string ans = "";
+        for(int i=0;i<n;i++){
+            ans+="$";
+        }  
+        while(i>0 && j>0){
+            if(a[i-1]==b[j-1]){
+                ans[index]=a[i-1];
+                i--;
+                j--;
+                index--;
+            }
+            else if(m[i][j]==m[i-1][j]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+        cout<<ans<<endl;
+        return n;
     }
 };
