fdf3ab187b85b31be7217405ea61285e3d250891
krishnaanandareddy <84138720+krishnaanandareddy@users.noreply.github.com>
2022-07-02 09:50:34 +0530

    Added solution - LeetHub

---
+++ Count the paths - GFG/count-the-paths.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+	int possible_paths(vector<vector<int>>edges, int n, int s, int d){
+	    // Code here
+	    vector<int> g[n];
+	    for(auto v:edges){
+	        g[v[0]].push_back(v[1]);
+	    }
+	    queue<int> q;
+	    int ans = 0;
+	    q.push(s);
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        if(node==d)ans++;
+	        for(auto i:g[node]){
+	            q.push(i);
+	        }
+	    }
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m, s, d;
+		cin >> n >> m >> s >> d;
+		vector<vector<int>>edges;
+		for(int i = 0; i < m; i++){
+			int u, v;
+			cin >> u >> v;
+			edges.push_back({u,v});
+		}
+		Solution obj;
+		int ans = obj.possible_paths(edges, n, s, d);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
