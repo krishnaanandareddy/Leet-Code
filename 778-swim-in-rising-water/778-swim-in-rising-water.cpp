a7f222ef6c6e6dd46bfbe5d8a94d7d54fb07d9df
krishnaanandareddy <84138720+krishnaanandareddy@users.noreply.github.com>
2022-07-03 00:27:51 +0530

    Time: 23 ms (78.43%), Space: 8.8 MB (86.05%) - LeetHub

---
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    bool allow(int m, int n, int x, int y){
+        if(x>=0 && y>=0 && x<m && y<n)return 1;
+        return 0;
+    }
+public:
+    int swimInWater(vector<vector<int>>& v) {
+        int m = v.size();
+        int n = v[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>> >,greater<pair<int,pair<int,int>>>> pq;
+        vector<vector<int>> dp(m,vector<int>(n,INT_MAX));
+        pq.push({0,{m-1,n-1}});
+        dp[m-1][n-1]=0;
+        vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!pq.empty()){
+            int dd = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            pq.pop();
+            for(auto d:dir){
+                int x = i+d.first;
+                int y = j+d.second;
+                if(allow(m,n,x,y)){
+                    int dc = dp[x][y];
+                    int temp = max(v[i][j],v[x][y]);
+                    temp=max(temp,dd);
+                    if(temp < dc){
+                        dp[x][y] = temp;
+                        pq.push({temp,{x,y}});
+                    }
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
