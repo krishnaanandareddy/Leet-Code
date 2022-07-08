3152d277939297d80f22797bcb52c32d3e82202c
krishnaanandareddy <84138720+krishnaanandareddy@users.noreply.github.com>
2022-07-08 23:31:55 +0530

    Time: 216 ms (62.62%), Space: 58.9 MB (5.49%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,18 +1,56 @@
 class Solution {
-public:
-    int longestConsecutive(vector<int> &num) {
-        int ss = num.size();
-        if(ss==0)return 0;
-        unordered_set<int> record(num.begin(),num.end());
-        int res = 1;
-        for(int n : num){
-            if(record.find(n)==record.end()) continue;
-            record.erase(n);
-            int prev = n-1,next = n+1;
-            while(record.find(prev)!=record.end()) record.erase(prev--);
-            while(record.find(next)!=record.end()) record.erase(next++);
-            res = max(res,next-prev-1);
+    unordered_map<int,int> m;
+    int find(int u,vector<int>&par){
+        if(par[u]==u)return u;
+        return par[u] = find(par[u],par);
+    }
+    void combine(int u, int v, vector<int>&par, vector<int>&rank){
+        u=find(u,par);
+        v=find(v,par);
+        if(u==v)return;
+        if(rank[u]>rank[v]){
+            par[v]=u;
         }
-        return res;
+        else if(rank[u]<rank[v]){
+            par[u]=v;
+        }
+        else{
+            par[u]=v;
+            rank[u]++;
+        }
+    }
+public:
+    int longestConsecutive(vector<int>& v) {
+        int n = v.size();
+        if(n==0)return 0;
+        vector<int>rank(n,1);
+        vector<int>par(n,-1);
+        for(int i=0;i<n;i++){
+            par[i]=i;
+            if(m[v[i]]==0){
+                m[v[i]]=i+1;
+            }
+            else{
+                continue;
+            }
+            int prev = v[i]-1;
+            int next = v[i]+1;
+            if(m[prev]!=0){
+                int pind = m[prev]-1;
+                combine(i,pind,par,rank);
+            }
+            if(m[next]!=0){
+                int nind = m[next]-1;
+                combine(i,nind,par,rank);
+            }
+        }
+        unordered_map<int,int> freq;
+        int ans = 1;
+        for(int i=0;i<n;i++){
+            int p = find(i,par);
+            freq[p]++;
+            ans = max(ans,freq[p]);
+        }
+        return ans;
     }
 };
