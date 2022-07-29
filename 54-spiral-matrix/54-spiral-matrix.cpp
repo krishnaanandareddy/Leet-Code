0822c1883bda494efe4483b95dbcb5bd370a2987
krishnaanandareddy <84138720+krishnaanandareddy@users.noreply.github.com>
2022-07-29 19:29:47 +0530

    Time: 0 ms (100.00%), Space: 7 MB (30.15%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,106 +1,27 @@
 class Solution {
-    bool r=0;
-    bool d=0;
-    bool l=0;
-    bool u=0;
-    vector<int> next(int x,int y,vector<vector<int>>& v,int count){
-        if(l&& d&& r&& u && count>=4)return {x,y};
-        if(r==1){
-            vector<int> temp = nextr(x,y,v);
-            count++;
-            if(temp[1]!=y){
-                r=1;d=0;l=0;u=0;
-                return {x,y+1};
-            }
-            d=1;
-        }
-        if(d==1){
-            vector<int> temp = nextd(x,y,v);
-            count++;
-            if(temp[0]!=x){
-                r=0;d=1;l=0;u=0;
-                return {x+1,y};
-            }
-            l=1;
-        }
-        if(l==1){
-            vector<int> temp = nextl(x,y,v);
-            count++;
-            if(temp[1]!=y){
-                r=0;d=0;l=1;u=0;
-                return {x,y-1};
-            }
-            u=1;
-        }
-        if(u==1){
-            vector<int> temp = nextu(x,y,v);
-            cout<<temp[0]<<temp[1]<<endl;
-            count++;
-            if(temp[0]!=x){
-                r=0;d=0;l=0;u=1;
-                return {x-1,y};
-            }
-            r=1;
-        }
-        return next(x,y,v,count);
-        
-    }
-    vector<int> nextr(int x,int y,vector<vector<int>>& v){
-        int n = v.size();
-        int m = v[0].size();
-        if(y+1 < m && v[x][y+1]!=INT_MAX){
-            return{x,y+1};
-        }
-        else{
-            return {x,y};
-        }
-    }
-    vector<int> nextd(int x,int y,vector<vector<int>>& v){
-        int n = v.size();
-        int m = v[0].size();
-        if(x+1 < n && v[x+1][y]!=INT_MAX){
-            return{x+1,y};
-        }
-        else{
-            return {x,y};
-        }
-    }
-    vector<int> nextl(int x,int y,vector<vector<int>>& v){
-        int n = v.size();
-        int m = v[0].size();
-        if(y-1 >=0 && v[x][y-1]!=INT_MAX){
-            return{x,y-1};
-        }
-        else{
-            return {x,y};
-        }
-    }
-    vector<int> nextu(int x,int y,vector<vector<int>>& v){
-        int n = v.size();
-        int m = v[0].size();
-        if(x-1 >=0 && v[x-1][y]!=INT_MAX){
-            return{x-1,y};
-        }
-        else{
-            return {x,y};
-        }
-    }
-    
 public:
     vector<int> spiralOrder(vector<vector<int>>& v) {
         int n = v.size();
         int m = v[0].size();
+        vector<vector<int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
+        int curr = 0;
+        int i=0,j=0;
         vector<int> ans;
-        int i = 0;
-        int j = 0;
-        r=1;
         while(v[i][j]!=INT_MAX){
             ans.push_back(v[i][j]);
             v[i][j]=INT_MAX;
-            vector<int> temp = next(i,j,v,0);
-            i=temp[0];
-            j=temp[1];
-            cout<<i<<" "<<j<<" "<<v[i][j]<<endl;
+            int t = 4;
+            while(t--){
+                curr = curr%4;
+                int x = dir[curr][0]+i;
+                int y = dir[curr][1]+j;
+                if(x>=0 && x<n && y>=0 && y<m && v[x][y]!=INT_MAX){
+                    i=x;
+                    j=y;
+                    break;
+                }
+                curr++;
+            }
         }
         return ans;
     }
